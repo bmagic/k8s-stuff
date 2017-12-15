ca4f4228d4a109193d2ae07b1b7cc263ce471f61
Baptiste <baptiste.routhieau@gmail.com>
2017-12-15 18:08:53 +0100

    WIP

--- src/instance-creator.js
+++ src/instance-creator.js
@@ -1,21 +1,24 @@
-const { events } = require('./core/constants')
+const { events, states } = require('./core/constants')
 const ovhUtils = require('./utils/ovh-utils')
+var ObjectID = require('mongodb').ObjectID
 
 module.exports.start = () => {
+  const collection = global.db.collection('nodes')
+
   /**
    * ON EVENT ADD_NODE
    */
   global.emitter.on(events.add_node, async (id) => {
     global.logger.info({module: 'instance-creator'}, `The instance ${id} is in state init. I will ask ovh for it.`)
     try {
-      await ovhUtils.addInstance()
+      const instance = await ovhUtils.addInstance(id)
+      await collection.updateOne({_id: ObjectID(id)}, {$set: {state: states.adding, data: instance}})
     } catch (error) {
       global.logger.error({module: 'instance-creator', err: error})
-      global.emitter.emit(events.send_mail, 'Error in instance-creator', err.message)
+      global.emitter.emit(events.send_mail, 'Error in instance-creator', error.message)
     }
-    // TODO ASK OVH TO ADD INSTANCE
-    // TODO Change the node state to ADDING
   })
 
 // TODO check every 10 sec the state of the instance on OVH API
 }
+
--- src/utils/ovh-utils.js
+++ src/utils/ovh-utils.js
@@ -12,22 +12,41 @@ const ovh = require('ovh')({
   consumerKey: OVH_API_CONSUMER_KEY
 })
 
-module.exports.addInstance = async function () {
-  const flavorId = await getFlavor()
-  console.log(flavorId)
+module.exports.addInstance = async function (id) {
+  const flavorId = await getAvailableFlavorId()
+  const instance = await postInstance(id, flavorId)
+  return instance
 }
 
-async function getFlavor () {
-  // G1-15 : 989a9a6c-2404-472b-8530-f4cd42abf98d
-  // G1-30 : 31c9e2b5-24dc-4ce3-b851-1d130acb7eed
-  const flavorIds = ['989a9a6c-2404-472b-8530-f4cd42abf98d', '31c9e2b5-24dc-4ce3-b851-1d130acb7eed']
+async function getAvailableFlavorId () {
+  // S1-2 (2Go RAM 1vCore 0.012€/H) : eeb4ccc9-faa0-4afb-955e-6a0224f93055
+  // G1-15 (15Go RAM 4vCore GTX1070 0.478€/H) : 989a9a6c-2404-472b-8530-f4cd42abf98d
+  // G1-30 (30Go RAM 8vCore GTX1070 0.653€/H) : 31c9e2b5-24dc-4ce3-b851-1d130acb7eed
+  const flavorIds = ['eeb4ccc9-faa0-4afb-955e-6a0224f93055', '989a9a6c-2404-472b-8530-f4cd42abf98d', '31c9e2b5-24dc-4ce3-b851-1d130acb7eed']
   for (let flavorId of flavorIds) {
-    global.logger.debug(`OVH API GET /cloud/project/${OVH_CLOUD_PROJECT_ID}/flavor/${flavorId}`)
+    global.logger.debug({module: 'ovh-utils'}, `OVH API GET /cloud/project/${OVH_CLOUD_PROJECT_ID}/flavor/${flavorId}`)
     const flavor = await ovh.requestPromised('GET', `/cloud/project/${OVH_CLOUD_PROJECT_ID}/flavor/${flavorId}`)
     if (flavor.available) {
-      global.logger.debug({module: 'instance-creator'}, `Flavor ${flavor.id} is available`)
+      global.logger.debug({module: 'ovh-utils'}, `Flavor ${flavor.id} is available`)
       return flavor.id
     }
   }
   throw new Error('OUPS none of the flavors are available....')
+}
+
+async function postInstance (id, flavorId) {
+  global.logger.debug({module: 'ovh-utils'}, `OVH API POST /cloud/project/${OVH_CLOUD_PROJECT_ID}/instance`)
+  const instance = await ovh.requestPromised('POST', `/cloud/project/${OVH_CLOUD_PROJECT_ID}/instance`, {
+    serviceName: OVH_CLOUD_PROJECT_ID,
+    flavorId: flavorId,
+    imageId: 'ea97b799-40eb-4f0e-a385-b25f647142c3',
+    name: id,
+    networks: [
+      {networkId: 'bc63b98d13fbba642b2653711cc9d156ca7b404d2df009f7227172d37b5280a6'},
+      {networkId: 'pn-10198_0'}
+    ],
+    sshKeyId: '516d467764476c7a6447553d',
+    region: OVH_REGION
+  })
+  return instance
 }
